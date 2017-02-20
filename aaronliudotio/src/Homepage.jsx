d0f3eb13727ccb43afcb673669424fc618150fa5
Liu <aaron_liu@intuit.com>
2017-02-20 10:40:38 -0700

    Initial project by using create-react-app, install babel preset and es2015, add homepage.jsx

---
+++ aaronliudotio/src/Homepage.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div>This is a homepage</div>
+        );
+    }
+}
+
+export default Homepage;
